6738179214a8023ee0add64f723b36bedaad437f
yimajo <y.imajo@gmail.com>
2020-01-01 13:07:06 +0900

    Add code comment.

--- Sample1A/Common/Protocol/UseCase.swift
+++ Sample1A/Common/Protocol/UseCase.swift
@@ -21,26 +21,34 @@ protocol UseCase where Failure: Error {
 }
 
 final class AnyUseCase<Parameters, Success, Failure: Error>: UseCase {
+    // UseCaseを保持するboxを保持する
     private let box: AnyUseCaseBox<Parameters, Success, Failure>
 
+    // UseCaseの実体を初期化時に渡しboxを作成してそれを保持する
     init<T: UseCase>(_ base: T) where T.Parameters == Parameters,
                                       T.Success == Success,
                                       T.Failure == Failure {
         box = UseCaseBox<T>(base)
     }
 
+    // 外部から実行されると内部のboxにその処理を伝える
     func execute(
         _ parameters: Parameters,
         completion: ((Result<Success, Failure>) -> ())?
     ) {
         box.execute(parameters, completion: completion)
     }
+
     func cancel() {
         box.cancel()
     }
 }
 
+// AnyUseCaseさえ知ってればいい情報なためprivate extensionとしている
 private extension AnyUseCase {
+
+    // ジェネリクスの型パラメータParameters, Success, Failureを用いてメソッドをUseCaseと合わせる。
+    // UseCaseBoxに継承させて型パラメータの決定を行う。
     class AnyUseCaseBox<Parameters, Success, Failure: Error> {
         func execute(
             _ parameters: Parameters,
@@ -54,7 +62,8 @@ private extension AnyUseCase {
         }
     }
 
-    // Parameters, Success を UseCase のそれと合わせるために AnyUseCaseBox を継承する
+    // ジェネリクスの型パラメータとしてT: UseCaseを持つクラスとして定義。
+    // AnyUseCaseBoxを継承しAnyUseCaseBoxのパラメータとUseCaseの型を合わせる。
     final class UseCaseBox<T: UseCase>
         : AnyUseCaseBox<T.Parameters, T.Success, T.Failure>
     {
