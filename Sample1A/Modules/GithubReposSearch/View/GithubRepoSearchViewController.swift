66c3b6208ccf919ddc7bffbef5e25a82ee016ddb
yimajo <y.imajo@gmail.com>
2020-01-06 07:27:25 +0900

    Tweak

--- Sample1A/Modules/GithubReposSearch/View/GithubRepoSearchViewController.swift
+++ Sample1A/Modules/GithubReposSearch/View/GithubRepoSearchViewController.swift
@@ -75,7 +75,8 @@ extension GithubRepoSearchViewController: UITableViewDataSource {
         displayData.numberOfSections
     }
 
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView,
+                   titleForHeaderInSection section: Int) -> String? {
         displayData.title(of: section)
     }
 
@@ -83,7 +84,8 @@ extension GithubRepoSearchViewController: UITableViewDataSource {
         displayData.numberOfItems(in: section)
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let entity = displayData.item(with: indexPath)!
         if let stargazersCount = entity.stargazersCount {
